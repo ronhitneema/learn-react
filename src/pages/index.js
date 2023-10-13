47b8d9146096dca4c1fc2d56e5a0034543926fb7
ronhitneema <ronhitn@gmail.com>
2023-10-12 20:35:58 -0400

    worked inclass

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile.js'
-
+//import Profile from './components/profile.js'
+import Gallery from './components/gallery.js'
+import Profile from './qcomps/profile_mistake.js'
+import FComp from './qcomps/firstcomp.js'
+import Bio from './qcomps/bios.js'
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Profile/>
+        <Gallery/>
+        <FComp/>
+        <Bio/>
     </div>
   )
 }
