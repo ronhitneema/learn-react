a4036dbdc301efd12c5ad0018990028bb3f9397f
ronhitneema <ronhitn@gmail.com>
2023-10-19 19:52:57 -0400

    commit

--- src/pages/qcomps/gallery_props.js
+++ src/pages/qcomps/gallery_props.js
@@ -1,16 +1,3 @@
-function People({person, size}) {
-  return (
-    <img
-      className="avatar"
-      src={'https://i.imgur.com/' + person.imageId + 's.jpg'}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
-  );
-}
-
-
 export default function Gallery() {
   return (
     <div>
